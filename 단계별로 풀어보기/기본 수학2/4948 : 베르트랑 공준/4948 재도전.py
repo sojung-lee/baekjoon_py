a5dba2532b6144a016017aa4305c5b0aa1dfa191
sojung-lee <skyhigh01237@gmail.com>
2021-07-05 15:51:19 +0900

    baekjoon

--- 단계별로 풀어보기/기본 수학2/4948 : 베르트랑 공준/4948 재도전.py
+++ 단계별로 풀어보기/기본 수학2/4948 : 베르트랑 공준/4948 재도전.py
@@ -1,8 +1,25 @@
-import math
+def prime_num():
+    nums = [i for i in range(2,123456*2+1)]
+    prime_arr = []
+    for num in nums:
+        flag = True
+        for i in range(2, int(num**0.5)+1):
+            if num%i==0:
+                flag = False
+                break
+        if flag:
+            prime_arr.append(num)
+    return prime_arr
+
+arr = prime_num()
 while True:
     n = int(input())
-    if n == 0 :
+    if n == 0:
         break
-
-    count = 0
-    check = [True]*(2*n + 1)
+    cnt = 0
+    for a in arr:
+        if a>2*n:
+            break
+        elif n<a<=2*n:
+            cnt += 1
+    print(cnt)
