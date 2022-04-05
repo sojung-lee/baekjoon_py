dfc1176e926f1e8677c6657b9f333fa5ff826def
sojung-lee <skyhigh01237@gmail.com>
2022-04-06 00:01:04 +0900

    baekjoon

--- 단계별로 풀어보기/올인원 수업/ch08. 동적 프로그래밍 - 02. 핵심 유형 문제풀이/1495. 기타리스트.py
+++ 단계별로 풀어보기/올인원 수업/ch08. 동적 프로그래밍 - 02. 핵심 유형 문제풀이/1495. 기타리스트.py
@@ -4,3 +4,29 @@
 
 # 핵심 아이디어 : 모든 볼륨에 대하여 연주 가능 여부를 계산하기
 # D[i][j+1] = i번째 노래일 때 j 크기의 볼륨으로 연주 가능한지 여부
+# 노래를 순서대로 확인하며, 매 번 모든 크기의 볼륨에 대하여 검사합니다.
+# D[i][j-V[i]] = True   if D[i-1][j] = True
+# D[i][j+V[i]] = True   if D[i-1][j] = True
+
+n, s, m = map(int, input().split())
+array = list(map(int, input().split()))
+
+dp = [[0] * (m + 1) for _ in range(n + 1)]
+dp[0][s] = 1
+
+for i in range(1, n + 1):
+    for j in range(m + 1):
+        if dp[i - 1][j] == 0:
+            continue
+        if j - array[i - 1] >= 0:
+            dp[i][j - array[i - 1]] = 1
+        if j + array[i - 1] <= m:
+            dp[i][j + array[i - 1]] = 1
+
+result = -1
+for i in range(m, -1, -1):
+    if dp[n][i] == 1:
+        result = i
+        break
+
+print(result)
---
+++ 단계별로 풀어보기/올인원 수업/ch08. 동적 프로그래밍 - 02. 핵심 유형 문제풀이/2655. 가장 높은 탑 쌓기.py
@@ -0,0 +1,41 @@
+# 가장 긴 증가하는 부분 수열(LIS) 문제의 심화 변형 문제입니다.
+# 벽돌의 수가 N개일 때, 시간 복잡도 O(N**2)으로 해결할 수 있습니다.
+# 벽돌의 번호를 출력해야 한다는 점에서, 계산된 테이블을 역추적할 수 있어야 합니다.
+
+# 가장 먼저 벽돌을 무게 기준으로 정렬합니다.
+# D[i] = 인덱스가 i인 벽돌을 가장 아래에 두었을 때의 최대 높이
+# 각 벽돌에 대해서 확인하며 D[i]를 갱신합니다.
+# 모든 0 <= j < i 에 대하여, D[i] = max(D[i],D[j] + height[i]) if area[i] > area[j]
+
+n = int(input())
+array = []
+
+array.append((0,0,0,0))
+for i in range(1, n + 1)
+    area, height, weight = map(int, input().split())
+    array.append((i, area, height, weight))
+
+# 무게 기준 정렬
+array.sort(key = lambda data: data[3])
+dp = [0] * (n + 1)
+
+for i in range(1, n + 1):
+    for j in range(0, i):
+        if array[i][1] > array[j][1]: # 너비가 더 클 경우
+            dp[i] = max(dp[i], dp[j] + array[i][2])
+
+# 역추적
+max_value = max(dp)
+index = n
+result = []
+
+while index != 0:
+    if max_value == dp[index]:
+        result.append(array[index][0])
+        max_value -= array[index][2]
+
+    index -= 1
+
+result.reverse()
+print(len(result))
+[print(i) for i in result]
