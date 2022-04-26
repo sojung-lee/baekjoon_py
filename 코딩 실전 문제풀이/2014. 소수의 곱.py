ab17fe042954263c4943873c8ac4e2db6402c24c
sojung-lee <skyhigh01237@gmail.com>
2022-04-26 16:40:11 +0900

    coding

---
+++ 코딩 실전 문제풀이/2014. 소수의 곱.py
@@ -0,0 +1,24 @@
+import heapq
+import copy
+import sys
+input = sys.stdin.readline
+
+K, N = map(int, input().split())
+p_list = list(map(int, input().split()))
+
+lst, ck = copy.deepcopy(p_list), set()
+
+heapq.heapify(lst)
+ith = 0
+
+while ith < N:
+    mn = heapq.heappop(lst)
+    if mn in ck:
+        continue
+    ith += 1
+    ck.add(mn)
+    for i in p_list:
+        if mn * i < 2 ** 32:
+            heapq.heappush(lst, mn*i)
+
+print(mn)
